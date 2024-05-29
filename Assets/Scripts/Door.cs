3ffbed67879ded1435884dd8984d609659ae2d13
Felix Grimm <flx.grimm@gmail.com>
2024-05-29 20:54:52 +0200

    #3 - Let Player Enter Building

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject indoor;
+    public GameObject outdoor;
+
+    private bool _isIndoor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"{other.gameObject.tag} - {other.gameObject.name}");
+        if (!other.gameObject.CompareTag("Player")) return;
+
+        if (_isIndoor)
+        {
+            indoor.SetActive(false);
+            outdoor.SetActive(true);
+            _isIndoor = false;
+        }
+        else
+        {
+            indoor.SetActive(true);
+            outdoor.SetActive(false);
+            _isIndoor = true;
+        }
+    }
+}
